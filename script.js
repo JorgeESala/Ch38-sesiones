c05c75169ca2d01b5ef3b4af310e8f9ca04891b5
Jorge Sala <jorge.e.sala@gmail.com>
2024-02-19 14:00:04 -0500

    feat: funcion saludar persona

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo");
+// Funcion que envie un saludo con alert
+function saludo(){
+    alert("Hola mundo");
+}
+/* Pedir el nombre de la persona usando prompt ()
+ Mostrar un saludo y nombre de la persona con alert()
+ */
+function saludarPersona(){
+    let nombre = prompt("Ingrese nombre");
+    alert("Hola "+ nombre +" ten un buen día");
+}
